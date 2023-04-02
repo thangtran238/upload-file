5343dec36d0dfb239a66995dc83e88bbce085b2d
thangtran238 <116275141+thangtran238@users.noreply.github.com>
2023-04-02 21:17:53 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var mockData = fetch("https://fakestoreapi.com/products")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    var products = document.querySelector(".products");
+
+    products.innerHTML = ``;
+    data.forEach((item) => {
+      let newProduct = document.createElement("div");
+      newProduct.classList.add("product");
+      newProduct.innerHTML = `
+        <img src="${item.image}" alt="">
+        <div class="info">
+          <div class="title">${item.title}</div>
+          <div class="price">${item.price}$</div>
+        </div>`;
+      products.appendChild(newProduct);
+    });
+  });
+
+
+
+var searchInput = document.querySelector('.search input')
+searchInput.addEventListener('input', function(e) {
+  let txtSearch = e.target.value.trim().toLowerCase()
+  let listProductDOM = document.querySelectorAll('.product')
+  listProductDOM.forEach(item => {
+    if (item.innerText.toLowerCase().includes(txtSearch)) {
+      item.classList.remove('hide')
+    } else {
+      item.classList.add('hide')
+    }
+  })
+})
